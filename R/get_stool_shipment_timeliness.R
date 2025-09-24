a22fc274ba69ecb73aa1b3871b1feb6c436040d5
Mervin Keith Cuadera <40894971+mcuadera@users.noreply.github.com>
2025-09-24 12:05:32 -0400

    add trend and expand to include all countries

--- R/get_stool_shipment_timeliness.R
+++ R/get_stool_shipment_timeliness.R
@@ -17,22 +17,40 @@
 get_stool_shipment_timeliness <- function(lab_data, end_date = Sys.Date()) {
 
   end_date <- lubridate::as_date(end_date)
+  month_end_date <- lubridate::month(end_date, TRUE)
 
   summary <- lab_data |>
     dplyr::mutate(month = lubridate::month(CaseDate, label = TRUE),
                   days.collect.rec.lab = as.numeric(DateStoolReceivedinLab - DateStoolCollected)) |>
     dplyr::filter(year >= lubridate::year(end_date) - 1,
-                  month %in% format(seq(lubridate::floor_date(end_date, unit = "years"),
-                                        end_date,
-                                        by = "months"), format = "%b"),
+                  month < month_end_date,
                   !is.na(days.collect.rec.lab),
                   dplyr::between(days.collect.rec.lab, 0, 365)) |>
     dplyr::group_by(whoregion, country, culture.itd.cat, year, month) |>
     dplyr::summarize(median = median(days.collect.rec.lab, na.rm = TRUE), .groups = "drop") |>
-    dplyr::arrange(year) |>
+    dplyr::arrange(year)
+
+  complete_dataset <- tidyr::expand_grid(
+    country = unique(lab_data$country),
+    year = unique(summary$year),
+    month = unique(summary$month)
+    ) |>
+    dplyr::left_join(lab_data |>
+                       dplyr::select(country, whoregion, culture.itd.cat) |>
+                       dplyr::distinct())
+
+  summary <- dplyr::full_join(complete_dataset, summary)
+  summary <- summary |>
     tidyr::pivot_wider(names_from = year, values_from = median)
 
-  cli::cli_alert_info("Note: If end date is not the month end, comparisons from the previous year may be inaccurate")
+  summary["comparison"] <- summary[, 6] - summary[, 5]
+  summary <- summary |>
+    dplyr::mutate(trend = dplyr::case_when(
+      comparison == 0 ~ "Same",
+      comparison > 0 ~ "Increase",
+      comparison < 0 ~ "Decrease",
+      .default = "No data from both years"
+    ))
 
   return(summary)
 
