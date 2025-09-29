a45988ff68bdd08432c6da77c34511f5e5af331e
Mervin Keith Cuadera <40894971+mcuadera@users.noreply.github.com>
2025-09-29 10:13:40 -0400

    add proportion for lab pending

--- R/get_proportion_lab_pending.R
+++ R/get_proportion_lab_pending.R
@@ -18,12 +18,15 @@
 get_proportion_lab_pending <- function(afp_data, end_date = Sys.Date()) {
 
   summary <- afp_data |>
-    dplyr::filter(stooltolabdate >= lubridate::floor_date(end_date - months(3), unit = "months"),
-                  cdc.classification.all2 == "LAB PENDING") |>
+    dplyr::filter(stooltolabdate >= lubridate::floor_date(end_date - months(3), unit = "months")) |>
     dplyr::mutate(month = lubridate::month(stooltolabdate, label = TRUE),
                   year = lubridate::year(stooltolabdate)) |>
     dplyr::group_by(whoregion, country = place.admin.0, year, month) |>
-    dplyr::summarize(pending_samples = dplyr::n(), .groups = "drop")
+    dplyr::summarize(pending_samples = sum(cdc.classification.all2 == "LAB PENDING", na.rm = TRUE),
+                     prop_pending_samples = round(pending_samples / dplyr::n() * 100),
+                     prop_label = paste0(prop_pending_samples, " (", pending_samples, "/", dplyr::n(), ")"),
+                     .groups = "drop") |>
+    dplyr::select(-prop_pending_samples)
 
   return(summary)
 
