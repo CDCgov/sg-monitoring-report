3902c472b2d8a5b828f5fef1b80e7794d0b0cb3b
Mervin Keith Cuadera <40894971+mcuadera@users.noreply.github.com>
2025-12-10 09:54:02 -0500

    Create generate_performance_tile_plot.R
    Draft of function skeleton

---
+++ R/generate_performance_tile_plot.R
@@ -0,0 +1,52 @@
+generate_perfomance_tile_plot <- function(afp_cases_reported,
+                                          lab_pending,
+                                          afp_wpv_vdpv,
+                                          negative_lab_processing,
+                                          afp_shipment_timeliness,
+                                          afp_lab_processing,
+                                          prop_60,
+                                          es_shipment,
+                                          es_wpv_vdpv,
+                                          es_sites,
+                                          es_site_samples,
+                                          lab_intervals
+                                          ) {
+
+  # AFP ----
+  ## Number of AFP cases reported ----
+
+  ## Proportion of 60-days follow-up done ----
+
+  ## Proportion of lab pending ----
+
+  ## Timeliness of AFP VDPV/WILD detection ----
+
+  ## Timeliness of negative samples ----
+
+  ## Timeliness of stool specimen shipment ----
+
+  ## Timeliness of lab processing - AFP ----
+
+  # ES ----
+  ## Timeliness of ES shipment ----
+
+  ## Timeliness for ES VDPV/WPV detection ----
+
+  ## Number of operational ES sites ----
+
+  ## Number of samples per site ----
+
+  # Lab ----
+  ## Timeliness of virus isolation results ----
+
+  ## Timeliness of ITD results ----
+
+  ## Timeliness of shipment for sequencing ----
+
+  ## Timeliness of sequencing results ----
+
+  ## Lab workload ----
+
+
+
+}
