90b548c74c1c233f98d25d4570d7ea9fcf6bb4cc
Mervin Keith Cuadera <40894971+mcuadera@users.noreply.github.com>
2025-12-17 15:42:30 -0500

    Ensure that the lab pending samples are complete for all countries

--- R/get_proportion_lab_pending.R
+++ R/get_proportion_lab_pending.R
@@ -21,6 +21,7 @@ get_proportion_lab_pending <- function(afp_data, end_date = Sys.Date()) {
     dplyr::filter(stooltolabdate >= lubridate::floor_date(end_date - months(3), unit = "months")) |>
     dplyr::mutate(month = lubridate::month(stooltolabdate, label = TRUE),
                   year = lubridate::year(stooltolabdate)) |>
+    dplyr::filter(year == lubridate::year(end_date)) |>
     dplyr::group_by(whoregion, country = place.admin.0, year, month) |>
     dplyr::summarize(pending_samples = sum(cdc.classification.all2 == "LAB PENDING", na.rm = TRUE),
                      prop_pending_samples = round(pending_samples / dplyr::n() * 100),
@@ -28,6 +29,27 @@ get_proportion_lab_pending <- function(afp_data, end_date = Sys.Date()) {
                      .groups = "drop") |>
     dplyr::select(-prop_pending_samples)
 
-  return(summary)
+  included_months <- lubridate::month(lubridate::floor_date(end_date - months(3), unit = "months")):
+    lubridate::month(lubridate::floor_date(end_date, unit = "months"))
+  included_months <- month.abb[included_months]
+
+  full_table <- tidyr::expand_grid(
+    country = unique(afp_data$place.admin.0),
+    year = unique(summary$year),
+    month = included_months
+  ) |>
+    dplyr::mutate(whoregion = get_region(country)) |>
+    dplyr::mutate(whoregion = dplyr::case_when(
+      country == "SAO TOME AND PRINCIPE" ~ "AFRO",
+      country == "CYPRUS" ~ "EMRO",
+      .default = whoregion
+    ))
+
+  final_summary <- full_table |>
+    dplyr::left_join(summary) |>
+    tidyr::replace_na(list(pending_samples = 0,
+                           prop_label = "0 (0/0)"))
+
+  return(final_summary)
 
 }
