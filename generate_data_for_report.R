b57289c82985cc1d24f1b489a199835ea74725f2
Mervin Keith Cuadera <40894971+mcuadera@users.noreply.github.com>
2026-01-07 15:34:09 -0500

    revise quarto template to use end date rather than hardcoded one

--- generate_data_for_report.R
+++ generate_data_for_report.R
@@ -121,13 +121,13 @@ who_regions <- c("AFRO", "AMRO", "EMRO", "EURO", "SEARO", "WPRO")
 # AFP
 lapply(who_regions, \(x) {generate_afp_tile_plot(afp_cases_reported, prop_60, lab_pending, prop_classified,
                                                  afp_wpv_vdpv, negative_lab_processing, afp_shipment_timeliness,
-                                                 afp_lab_processing, end_date = Sys.Date(), lab_end_date = max_lab_date, who_region = x)
+                                                 afp_lab_processing, end_date = end_date, lab_end_date = max_lab_date, who_region = x)
   ggsave(paste0("images/", x, "_afp_plot.jpg"), width = 14, height = 8)
   })
 
 # ES
 lapply(who_regions, \(x) {
-  generate_es_tile_plot(es_shipment, es_wpv_vdpv, es_sites, es_site_samples, end_date = Sys.Date(), who_region = x)
+  generate_es_tile_plot(es_shipment, es_wpv_vdpv, es_sites, es_site_samples, end_date = end_date, who_region = x)
   ggsave(paste0("images/", x, "_es_plot.jpg"), width = 14, height = 8)
   })
 
