3202808d213e3848d09faa75063b441304413401
Mervin Keith Cuadera <40894971+mcuadera@users.noreply.github.com>
2026-01-09 15:29:21 -0500

    revise lab pending to look at only samples greater than 90 days

--- R/get_proportion_lab_pending.R
+++ R/get_proportion_lab_pending.R
@@ -18,25 +18,20 @@
 get_proportion_lab_pending <- function(afp_data, end_date = Sys.Date()) {
 
   summary <- afp_data |>
-    dplyr::filter(stooltolabdate >= lubridate::floor_date(end_date %m-% months(3), unit = "months")) |>
+    # only look at samples greater than 90 days
+    dplyr::filter(stooltolabdate < lubridate::floor_date(end_date %m-% months(3), unit = "months")) |>
     dplyr::mutate(month = lubridate::month(stooltolabdate, label = TRUE),
                   year = lubridate::year(stooltolabdate)) |>
     dplyr::filter(year == lubridate::year(end_date)) |>
-    dplyr::group_by(whoregion, country = place.admin.0, year, month) |>
+    dplyr::group_by(whoregion, country = place.admin.0) |>
     dplyr::summarize(pending_samples = sum(cdc.classification.all2 == "LAB PENDING", na.rm = TRUE),
                      prop_pending_samples = round(pending_samples / dplyr::n() * 100),
                      prop_label = paste0(prop_pending_samples, " (", pending_samples, "/", dplyr::n(), ")"),
                      .groups = "drop") |>
     dplyr::select(-prop_pending_samples)
 
-  included_months <- lubridate::month(lubridate::floor_date(end_date %m-% months(3), unit = "months")):
-    lubridate::month(lubridate::floor_date(end_date, unit = "months"))
-  included_months <- month.abb[included_months]
-
   full_table <- tidyr::expand_grid(
-    country = unique(afp_data$place.admin.0),
-    year = unique(summary$year),
-    month = included_months
+    country = unique(afp_data$place.admin.0)
   ) |>
     dplyr::mutate(whoregion = get_region(country)) |>
     dplyr::mutate(whoregion = dplyr::case_when(
