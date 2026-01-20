fd12e88af7f17bbee1db1b10e64f74a531d69947
Mervin Keith Cuadera <40894971+mcuadera@users.noreply.github.com>
2026-01-20 13:29:37 -0500

    Add indicator descriptions

--- R/generate_es_tile_plot.R
+++ R/generate_es_tile_plot.R
@@ -80,11 +80,34 @@ process_es_performance <- function(es_shipment = NULL,
     dplyr::left_join(es_site_samples_filtered) |>
     tidyr::pivot_longer(cols = dplyr::starts_with("I", ignore.case = FALSE), values_to = "value", names_to = "indicator") |>
     dplyr::mutate(value = dplyr::if_else(is.na(value), "To Be Determined", value),
+                  indicator_description = case_when(
+                    indicator == "I9" ~ paste0("<br>Description:</br> median days between collection to received in lab.",
+                                               " Timely if received within 3 days for in-country lab and 7 days for international lab.",
+                                               "On target if median days less than or equal to targets.\n",
+                                               "<br>Timeframe:</br> month before the specified end date. (",
+                                               lubridate::floor_date(end_date, unit = "month") %m-% months(1), ")"),
+                    indicator == "I10" ~ paste0("<br>Description:</br> median days between collection to notification to HQ for WPV/VDPV samples.",
+                                                " On target if median is 35 days if in-country lab and 46 days if shipped to international lab.\n",
+                                                "<br>Timeframe:</br> month before the specified end date. (",
+                                                lubridate::floor_date(end_date, unit = "month") %m-% months(1), ")"),
+                    indicator == "I11" ~ paste0("<br>Description:</br> Proportion of operational sites for the current month compared to the same month last year.",
+                                                " Operational sites are sites with at least 3 collections in the last 12 months. ",
+                                                " On target if there is less than a 20% decrease compared to the same period last year.\n",
+                                                "<br>Timeframe:</br> current month of the end date versus the previous month (",
+                                                lubridate::month(end_date, label = TRUE), lubridate::year(end_date), " to ",
+                                                lubridate::month(end_date, label = TRUE), lubridate::year(end_date - 1), ")"),
+                    indicator == "I12" ~ paste0("<br>Description:</br> Percent change of median collections for operational sites from current month compared to the previous month.",
+                                                " On target if median collections from current month stays the same or increase compared to previous month.\n",
+                                                "<br>Timeframe</br> current month to the previous month (",
+                                                lubridate::month(end_date, label = TRUE), " to ",
+                                                lubridate::month(end_date %m-% months(1), label = TRUE), ")")
+                    ),
+
                   indicator = dplyr::case_when(
                     indicator == "I9" ~ "Timeliness of\nES shipment",
                     indicator == "I10" ~ "Timeliness of\nES WPV/VDPV detection",
                     indicator == "I11" ~ "Proportion operational\nES sites",
-                    indicator == "I12" ~ "Proportion of performant\nES active sites",
+                    indicator == "I12" ~ "Median number of collections among\nES active sites",
                     .default = indicator
                   ))
 
