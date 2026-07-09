c19f03b051fac351d6816d2015cd468761d9485b
Michelle Sloan <jtq4@cdc.gov>
2026-07-09 12:32:12 -0400

    Update generate_data_for_report.R

--- generate_data_for_report.R
+++ generate_data_for_report.R
@@ -84,8 +84,10 @@ es_prop_active_sites_collections$data <- add_risk(es_prop_active_sites_collectio
 
 
 # Lab Indicators ----
+lab_virus_isolation_timliness <- build_timeliness_virus_isolation_indicator(lab_data, max(lab_data$DateFinalCellCultureResult, na.rm = TRUE))
 
-# NEED TO ADD
+
+
 
 # Save and Cache -------------------------------------------------------------------------------------------------------
 
