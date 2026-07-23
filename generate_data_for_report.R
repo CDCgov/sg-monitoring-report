9fc83bcbebe561260b4cd7b8ad1f9315383ec33b
Andrea Stewart <55840298+als329@users.noreply.github.com>
2026-07-23 11:19:19 -0400

    New QMD and generate_data_for_report
    currently has placeholder tabs for summary tables

--- generate_data_for_report.R
+++ generate_data_for_report.R
@@ -71,10 +71,10 @@ afp_timely_stool$data <- add_risk(afp_timely_stool$data, "country")
 
 # ES Indicators ----
 # Generate indicator results
-es_active_sites <- build_number_of_active_ES_sites(raw_data$es, end_date-30)
-es_timely_shipment <- build_timeliness_of_es_shipment_indicator(raw_data$es, end_date-30)
-es_wpv_vdpv_timeliness <- build_timeliness_es_wpv_vdpv_notification_indicator(raw_data$es, end_date-30)
-es_prop_active_sites_collections <- build_prop_active_es_sites_with_monthly_collections(raw_data$es, end_date-30)
+es_active_sites <- build_number_of_active_ES_sites(raw_data$es, end_date)
+es_timely_shipment <- build_timeliness_of_es_shipment_indicator(raw_data$es, end_date)
+es_wpv_vdpv_timeliness <- build_timeliness_es_wpv_vdpv_notification_indicator(raw_data$es, end_date)
+es_prop_active_sites_collections <- build_prop_active_es_sites_with_monthly_collections(raw_data$es, end_date)
 
 # Add risk category
 es_active_sites$data <- add_risk(es_active_sites$data, "country")
